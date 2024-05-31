332574acd1edfcd4252628e6117f6b04c4ac78b3
Пудовкин Дмитрий <162864606+Wirx74@users.noreply.github.com>
2024-05-31 09:11:43 +0500

    Update build.sh

--- ITTI/build.sh
+++ ITTI/build.sh
@@ -1,15 +1,11 @@
+# Linux версия
 rm -r build/*
 mkdir build
 
-# Локальная версия
-g++ -I./include -I./src -I./itti_structs -I./itti_structs/asn1c_build -I./rapidjson -o build/main main.cpp src/AdmUeReleaseRequest.cpp src/PathSwitchRequest.cpp src/DrbAdditionalInfo.cpp src/InitialContextSetupAcknowledgement.cpp
+g++ -I./include -I./src -I./itti_structs -I./itti_structs/asn1c_build -I./rapidjson -o build/Server Server-Linux.cpp src/AdmUeReleaseRequest.cpp src/PathSwitchRequest.cpp src/DrbAdditionalInfo.cpp src/InitialContextSetupAcknowledgement.cpp src/InitialContextSetupResponse.cpp
+g++ -I./include -I./src -I./itti_structs -I./itti_structs/asn1c_build -I./rapidjson -o build/Client1 Client1-Linux.cpp src/AdmUeReleaseRequest.cpp src/PathSwitchRequest.cpp src/DrbAdditionalInfo.cpp src/InitialContextSetupAcknowledgement.cpp src/InitialContextSetupResponse.cpp
+g++ -I./include -I./src -I./itti_structs -I./itti_structs/asn1c_build -I./rapidjson -o build/Client2 Client2-Linux.cpp src/AdmUeReleaseRequest.cpp src/PathSwitchRequest.cpp src/DrbAdditionalInfo.cpp src/InitialContextSetupAcknowledgement.cpp src/InitialContextSetupResponse.cpp
 
-# Windows версия
-#g++ -I./include -I./src -I./itti_structs -I./itti_structs/asn1c_build -I./rapidjson -o build/main-v2 main-v2.cpp src/AdmUeReleaseRequest.cpp src/PathSwitchRequest.cpp src/DrbAdditionalInfo.cpp src/InitialContextSetupAcknowledgement.cpp
-
-# Linux версия
-g++ -I./include -I./src -I./itti_structs -I./itti_structs/asn1c_build -I./rapidjson -o build/main-v3 main-v3.cpp src/AdmUeReleaseRequest.cpp src/PathSwitchRequest.cpp src/DrbAdditionalInfo.cpp src/InitialContextSetupAcknowledgement.cpp
-
-#./build/main
-#./build/main-v2
-#./build/main-v3
+./build/Server
+./build/Client1
+./build/Client2
