17db922e48c826086d941c9a793afa211b43600a
Пудовкин Дмитрий <162864606+Wirx74@users.noreply.github.com>
2024-05-31 15:39:20 +0500

    Update build.sh

--- ITTI/build.sh
+++ ITTI/build.sh
@@ -1,11 +1,17 @@
 # Linux версия
+
+# Очищаем папку build
 rm -r build/*
+
+# Создаем папку build, если при отсутствии
 mkdir build
 
+# Создаем поочередно: Server, Client1, Client2.
 g++ -I./include -I./src -I./itti_structs -I./itti_structs/asn1c_build -I./rapidjson -o build/Server Server-Linux.cpp src/AdmUeReleaseRequest.cpp src/PathSwitchRequest.cpp src/DrbAdditionalInfo.cpp src/InitialContextSetupAcknowledgement.cpp src/InitialContextSetupResponse.cpp
 g++ -I./include -I./src -I./itti_structs -I./itti_structs/asn1c_build -I./rapidjson -o build/Client1 Client1-Linux.cpp src/AdmUeReleaseRequest.cpp src/PathSwitchRequest.cpp src/DrbAdditionalInfo.cpp src/InitialContextSetupAcknowledgement.cpp src/InitialContextSetupResponse.cpp
 g++ -I./include -I./src -I./itti_structs -I./itti_structs/asn1c_build -I./rapidjson -o build/Client2 Client2-Linux.cpp src/AdmUeReleaseRequest.cpp src/PathSwitchRequest.cpp src/DrbAdditionalInfo.cpp src/InitialContextSetupAcknowledgement.cpp src/InitialContextSetupResponse.cpp
 
+# Запускаем поочередно: Server, Client1, Client2.
 ./build/Server
 ./build/Client1
 ./build/Client2
